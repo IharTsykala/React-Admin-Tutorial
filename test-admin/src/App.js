2b97c41bc9e588117113accba42f9c539c57ca3e
Ihar Tsykala <you@example.comSudja09@bk.ru>
2019-12-26 19:50:08 +0300

    feat: first half tutorial

---
+++ test-admin/src/App.js
@@ -0,0 +1,20 @@
+// in src/App.js
+import React from 'react';
+
+import { Admin, Resource } from 'react-admin';
+import { PostList } from './posts';
+import jsonServerProvider from 'ra-data-json-server';
+import { UserList } from './users';
+
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+
+const App = () => (
+    <Admin dataProvider={dataProvider}>
+     
+      <Resource name="posts" list={PostList} />
+        <Resource name="users" list={UserList} />
+    </Admin>
+);
+
+export default App;
+
